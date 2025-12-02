7d82f0f6b67d712ddc4a0eb07fb2f61113f839e4
KUPALNIC <sashakupa2411@gmail.com>
2025-12-02 16:24:11 +0300

    3 labs

--- PythonLessons/LAB/scipy/ex2.py
+++ PythonLessons/LAB/scipy/ex2.py
@@ -8,14 +8,9 @@ with open('text1.txt', 'r') as f:
 N = int(lines[0].strip())
 A = np.array([list(map(float, lines[i].split())) for i in range(1, 1 + N)])
 b = np.array(list(map(float, lines[1 + N].split())))
-
 x = solve(A, b)
-
 plt.figure(figsize=(8, 5))
-plt.bar(range(len(x)), x, color='steelblue', edgecolor='black', alpha=0.8)
-plt.xlabel('i')
-plt.ylabel('x[i]')
-plt.title('Решение системы Ax = b')
-plt.grid(True, linestyle='--', alpha=0.5)
+plt.bar(range(len(x)), x)
+plt.grid()
 plt.xticks(range(len(x)))
 plt.show()
